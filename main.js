aed219ca1f8c242afa9ab30e24531ef23083db1f
antonio <ammj24@hotmail.com>
2019-10-25 03:03:17 +0100

    added legend and tooltip

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function time(t) {
     let time = new Date();
     time.setMinutes(t[0]);
     time.setSeconds(t[1]);
-    
+
     return time.getTime();
 }
 
@@ -18,7 +18,7 @@ function scatterPlotGraph(dataset) {
     d3.select('body')
         .append('h1')
         .attr('id', 'title')
-        .text('Doping in Professional Bycicle Racing')
+        .text('Doping in Professional Bicycle Racing')
 
     const width = 1000;
     const height = 500;
@@ -49,6 +49,20 @@ function scatterPlotGraph(dataset) {
                     .attr('width', width)
                     .attr('height', height)
 
+    let tooltip = d3.select('body')
+                      .append('div')
+                      .attr('width', '3rem')
+                      .attr('height', '2rem')
+                      .style('background-color', 'cadetblue')
+                      .style('color', 'white')
+                      .style('opacity', 0.95)
+                      .style('font-size', '0.9rem')
+                      .style('padding', '0.6rem')
+                      .style('border-radius', '4px')
+                      .attr('id', 'tooltip')
+                      .style('position', 'absolute')
+                      .style('visibility', 'hidden')
+
     svg.append('g')
         .attr('id', 'x-axis')
         .attr('transform', 'translate(0,' + (height - padding) + ')')
@@ -68,10 +82,31 @@ function scatterPlotGraph(dataset) {
         .attr('data-yvalue', (d) => {
             return new Date(time(d['Time'])).toISOString()
         })
-        .attr('r', '6')
+        .attr('r', '7')
         .attr('cx', (d) => xScale(d['Year']))
         .attr('cy', (d) => yScale(time(d['Time'])))
-    
+        .style('fill', 'cadetblue')
+        .style('opacity', 0.8)
+        .on('mouseover', function(d) {
+            tooltip.attr('data-year', d['Year'])
+            tooltip.style('visibility', 'visible')
+            tooltip.html( d['Name'] + ' - ' + d['Nationality'] + '<br>' 
+                        + d['Year'] + ' - ' + 'Time ' + d['Time'] + '<hr>'
+                        + d['Doping'])
+                   .style('top', padding + yScale(time(d['Time'])) + 3)
+                   .style('left', padding + xScale(d['Year']) + 52)
+        })
+        .on('mouseout', (d) => {
+            tooltip.style('visibility', 'hidden')
+        })
+        
+    d3.select('body')
+        .append('text')
+        .attr('id', 'legend')
+        .attr('x', 300)
+        .attr('y', 260)
+        .text('some aditional info...')
+
 }
 
 
